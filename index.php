c9c10463eadd6590ba151db0b5e2102d2f55de8a
ProsperityGH <cskins22@gmail.com>
2022-02-02 17:27:15 +0100

    Styling Index

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ include("connection.php");
 include("functions.php");
 
 $user_data = check_login($con);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,10 +13,14 @@ $user_data = check_login($con);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login geslaagd!</title>
+    <link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
-    <h1>Login succesful!</h1>
-    <h2>Welcome <?php echo $user_data["user_name"] ?></h2>
-    <a href="logout.php">Log out here</a>
+    <div id="box">
+        <img src="assets/img/checkmark.png" id="checkmark">
+        <h1>Login succesful!</h1>
+        <h2>Welcome <?php echo $user_data["user_name"] ?></h2>
+        <a href="logout.php">Log out here</a>
+    </div>
 </body>
 </html>
