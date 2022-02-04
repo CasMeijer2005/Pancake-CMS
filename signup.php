a25afe820d97bad1b507c84c925a86aecbb7a293
Tygo van Zon <98739915+TygoVZ@users.noreply.github.com>
2022-02-04 15:06:23 +0100

    Add files via upload

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 include("connection.php");
 include("functions.php");
@@ -7,6 +8,18 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     //SOMETHING WAS POSTED
     $user_name = $_POST['user_name'];
     $password = hash("sha256", $_POST['password']);
+
+    $check_duplicate_username = "SELECT `user_name` FROM `users` WHERE `user_name` = '$user_name' ";
+
+    $result = mysqli_query($con, $check_duplicate_username);
+
+    $count = mysqli_num_rows($result);
+
+    if($count > 0) {
+        echo '<label class="input">Username already taken!</label> <br>';
+
+        return false;
+    }
 
     if (!empty($user_name) && !empty($password)  && !is_numeric($user_name)) {
         //save to database
@@ -20,25 +33,29 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "please enter some valid information!";
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/login.css">
-    <link rel="icon" type="image/x-icon" href="assets/img/pancake.png">
     <title>Sign up</title>
 </head>
+
 <body>
     <form method="POST">
+        <label id="usernameTaken"></label>
         <input type="text" class="input" id="user_name" name="user_name" placeholder="Username">
         <input type="password" class="input" id="password" name="password" placeholder="Password">
         <input type="submit" id="submit" value="sign up">
     </form>
     <img src="assets/img/google.png" id="google" onclick="google()">
-    <a href="login.php" id="signup">Login here</a>
+    <a href="login.php" id="signup">login here</a>
     <script src="assets/js/login.js"></script>
 </body>
+
 </html>
