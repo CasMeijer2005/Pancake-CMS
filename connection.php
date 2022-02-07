c211801c58726e04fdcebecc5676555a3022f44a
Tygo van Zon <98739915+TygoVZ@users.noreply.github.com>
2022-02-07 10:18:48 +0100

    Commentaar toegevoegd

--- connection.php
+++ connection.php
@@ -5,6 +5,12 @@ $bduser = "root";
 $bdpass = "";
 $bdname = "pancakecms";
 
+// Runt als de connectie niet gelukt is
 if (!$con = mysqli_connect($bdhost, $bduser, $bdpass, $bdname)) {
     die("faild to connect!");
 }
+
+//      __0__/
+//     / /
+//    __/\
+//        |
