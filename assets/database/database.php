892a93e73d35ada1b72d27cddca271b4821b46eb
Tygo van Zon <98739915+TygoVZ@users.noreply.github.com>
2022-02-11 11:43:34 +0100

    Added some shit i guess idk

--- assets/database/database.php
+++ assets/database/database.php
@@ -32,27 +32,48 @@ $sql = "CREATE TABLE IF NOT EXISTS users (
     id bigint(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     `user_id` bigint(20) NOT NULL,
     `user_name` VARCHAR(100) NOT NULL,
-    `user_email` VARCHAR(100) NOT NULL,
     `password` VARCHAR(100) NOT NULL,
     `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
 // Check if table was created
 if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully <br>";
+    echo "Table users created successfully <br>";
 } else {
     echo "Error creating table: " . $conn->error . "<br>";
+}
+
+// Create table
+$sql = "CREATE TABLE IF NOT EXISTS content (
+    `title` TEXT NOT NULL,
+    `header1` TEXT NOT NULL,
+    `header2` TEXT NOT NULL,
+    `header3` TEXT NOT NULL,
+    `header4` TEXT NOT NULL,
+    `paragraph1` TEXT NOT NULL,
+    `paragraph2` TEXT NOT NULL,
+    `paragraph3` TEXT NOT NULL,
+    `paragraph4` TEXT NOT NULL
+)";
+
+// Check if table was created
+if ($conn->query($sql) === TRUE) {
+    echo "Table content created successfully <br>";
+} else {
+    echo "Error creating table: " . $conn->error . "<br>";
+}
+
+// Insert voor update
+$sql = "INSERT INTO content (`title`, `header1`, `header2`, `header3`, `header4`, `paragraph1`, `paragraph2`, `paragraph3`, `paragraph4`) VALUES (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ')";
+
+// Check if insertion was succesful
+if ($conn->query($sql) === TRUE) {
+    echo "Insertion succesful <br>";
+} else {
+    echo "Error inserting data: " . $conn->error . "<br>";
 }
 
 // Close connection
 $conn->close();
 
-// __/\\\\\\\\\\\\\____/\\\________/\\\__/\\\\\\\\\\\\\______________/\\\\\\\\\\\_____/\\\\\\\\\\\______________/\\\\\\\\\\\\\\\__/\\\________/\\\__/\\\\\_____/\\\_        
-//  _\/\\\/////////\\\_\/\\\_______\/\\\_\/\\\/////////\\\___________\/////\\\///____/\\\/////////\\\___________\/\\\///////////__\/\\\_______\/\\\_\/\\\\\\___\/\\\_       
-//   _\/\\\_______\/\\\_\/\\\_______\/\\\_\/\\\_______\/\\\_______________\/\\\______\//\\\______\///____________\/\\\_____________\/\\\_______\/\\\_\/\\\/\\\__\/\\\_      
-//    _\/\\\\\\\\\\\\\/__\/\\\\\\\\\\\\\\\_\/\\\\\\\\\\\\\/________________\/\\\_______\////\\\___________________\/\\\\\\\\\\\_____\/\\\_______\/\\\_\/\\\//\\\_\/\\\_     
-//     _\/\\\/////////____\/\\\/////////\\\_\/\\\/////////__________________\/\\\__________\////\\\________________\/\\\///////______\/\\\_______\/\\\_\/\\\\//\\\\/\\\_    
-//      _\/\\\_____________\/\\\_______\/\\\_\/\\\___________________________\/\\\_____________\////\\\_____________\/\\\_____________\/\\\_______\/\\\_\/\\\_\//\\\/\\\_   
-//       _\/\\\_____________\/\\\_______\/\\\_\/\\\___________________________\/\\\______/\\\______\//\\\____________\/\\\_____________\//\\\______/\\\__\/\\\__\//\\\\\\_  
-//        _\/\\\_____________\/\\\_______\/\\\_\/\\\________________________/\\\\\\\\\\\_\///\\\\\\\\\\\/_____________\/\\\______________\///\\\\\\\\\/___\/\\\___\//\\\\\_ 
-//         _\///______________\///________\///__\///________________________\///////////____\///////////_______________\///_________________\/////////_____\///_____\/////__
+?>
